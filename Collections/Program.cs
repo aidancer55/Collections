5face49400d375d4ec4e5997f1d3d6c4875010ff
aidancer55 <bluebonnets77@gmail.com>
2022-02-17 22:26:14 +0300

    Добавьте файлы проекта.

---
+++ Collections/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string cnt = Console.ReadLine();
+            Console.WriteLine(Check(cnt));
+            Console.ReadKey();
+        }
+        static bool Check(string cnt)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in cnt)
+            {
+                switch (c)
+                {
+                    case '{':
+                    case '(':
+                    case '[':
+                        stack.Push(c);
+                        break;
+
+                    case '}':
+                        if (stack.Count == 0)
+                            return false;
+                        if (stack.Pop() != '{')
+                            return false;
+                        break;
+                    case ']':
+                        if (stack.Count == 0)
+                            return false;
+                        if (stack.Pop() != '[')
+                            return false;
+                        break;
+                    case ')':
+
+                        if (stack.Count == 0)
+                            return false;
+                        if (stack.Pop() != '(')
+                            return false;
+                        break;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
